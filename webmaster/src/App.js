cda7209b17f39eb019b19dd51dfd9dd24a6eaccf
25sreddy <86977180+25sreddy@users.noreply.github.com>
2022-01-09 16:05:53 -0500

    navbar

--- webmaster/src/App.js
+++ webmaster/src/App.js
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
